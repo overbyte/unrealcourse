79ae01868c2921eefe31cf014c5f322e7b615961
Allandt Bik-Elliott <allandt.bik-elliott@imagination.com>
2020-04-08 15:15:44 +0100

    refactor to more closely match the tutorial structure add increasing difficulty loop add win/lose condition add more cool ascii art

--- section-2-triplex/triplex.cpp
+++ section-2-triplex/triplex.cpp
@@ -1,41 +1,95 @@
 #include <iostream>
 
-void PlayGame();
+bool PlayGame(int Difficulty);
+bool TestWinCondition();
 void OutputIntro();
-void OutputChallenge();
 void InputPlayerGuess();
-void TestWinCondition();
 void ResetStdIn();
 
-const int CodeA = 3;
-const int CodeB = 4;
-const int CodeC = 9;
-const int CodeSum = CodeA + CodeB + CodeC;
-const int CodeProduct = CodeA * CodeB * CodeC;
 const int TotalGuesses = 3;
 
 int GuessA, GuessB, GuessC;
 int GuessSum, GuessProduct;
 int CurrentGuess = 0;
-int isGameWon = false;
+int bLevelComplete = false;
 
 int main() 
 {
     OutputIntro();
 
-    while (CurrentGuess < TotalGuesses && !isGameWon) {
-        PlayGame();
+    const int MaxDifficulty = 5;
+    int LevelDifficulty = 1;
+
+    while (LevelDifficulty <= MaxDifficulty)
+    {
+        bLevelComplete = PlayGame(LevelDifficulty);
+
         ResetStdIn();
+
+        if (bLevelComplete)
+        {
+            CurrentGuess = 0;
+            ++LevelDifficulty;
+        }
     }
+
+    std::cout << "*** YOU DID IT!! Now grab your spoils and get out of there! ***\n\n";
+
+    std::cout << "    .o oOOOOOOOo                                            OOOo\n";
+    std::cout << "    Ob.OOOOOOOo  OOOo.      oOOo.                      .adOOOOOOO\n";
+    std::cout << "    OboO\"\"\"\"\"\"\"\"\"\"\"\".OOo. .oOOOOOo.    OOOo.oOOOOOo..\"\"\"\"\"\"\"\"\"'OO\n";
+    std::cout << "    OOP.oOOOOOOOOOOO \"POOOOOOOOOOOo.   `\"OOOOOOOOOP,OOOOOOOOOOOB'\n";
+    std::cout << "    `O'OOOO'     `OOOOo\"OOOOOOOOOOO` .adOOOOOOOOO\"oOOO'    `OOOOo\n";
+    std::cout << "    .OOOO'            `OOOOOOOOOOOOOOOOOOOOOOOOOO'            `OO\n";
+    std::cout << "    OOOOO                 '\"OOOOOOOOOOOOOOOO\"`                oOO\n";
+    std::cout << "   oOOOOOba.                .adOOOOOOOOOOba               .adOOOOo.\n";
+    std::cout << "  oOOOOOOOOOOOOOba.    .adOOOOOOOOOO@^OOOOOOOba.     .adOOOOOOOOOOOO\n";
+    std::cout << " OOOOOOOOOOOOOOOOO.OOOOOOOOOOOOOO\"`  '\"OOOOOOOOOOOOO.OOOOOOOOOOOOOO\n";
+    std::cout << " \"OOOO\"       \"YOoOOOOMOIONODOO\"`  .   '\"OOROAOPOEOOOoOY\"     \"OOO\"\n";
+    std::cout << "    Y           'OOOOOOOOOOOOOO: .oOOo. :OOOOOOOOOOO?'         :`\n";
+    std::cout << "    :            .oO\%OOOOOOOOOOo.OOOOOO.oOOOOOOOOOOOO?         .\n";
+    std::cout << "    .            oOOP\"\%OOOOOOOOoOOOOOOO?oOOOOO?OOOO\"OOo\n";
+    std::cout << "                 '\%o  OOOO\"\%OOOO\%\"\%OOOOO\"OOOOOO\"OOO':\n";
+    std::cout << "                      `$\"  `OOOO' `O\"Y ' `OOOO'  o             .\n";
+    std::cout << "    .                  .     OP\"          : o     .\n";
+    std::cout << "                              :\n";
+    std::cout << "                              .\n\n\n";
 
     return 0; 
 }
 
-void PlayGame()
+bool PlayGame(int Difficulty)
 {
+    const int CodeA = rand() % Difficulty + 1;
+    const int CodeB = rand() % Difficulty + 1;
+    const int CodeC = rand() % Difficulty + 1;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
+
     CurrentGuess++;
+
+    // InitialiseVariables
+    std::cout << "This is a level " << Difficulty << " secure room\n";
+    std::cout << "There are 3 numbers in the code\n";
+    std::cout << "The numbers add up to " << CodeSum << std::endl;
+    std::cout << "The numbers multiply to " << CodeProduct << std::endl;
+
     InputPlayerGuess();
-    TestWinCondition();
+
+    if  (GuessSum == CodeSum && GuessProduct == CodeProduct)
+    {
+        std::cout << "\n>>> Congratulations! You got it right! <<<\n\n";
+
+        return true;
+    }
+    else
+    {
+        std::cout << "\n>>> That wasn't it! <<<\n\n";
+        const int GuessesRemaining = TotalGuesses - CurrentGuess;
+        std::cout << "There are " << GuessesRemaining << " attempts left! ";
+
+        return false;
+    }
 }
 
 // output a static intro
@@ -51,24 +105,22 @@ void OutputIntro()
     std::cout << "               !:~~~ .:!M\"T#$$$$WX??#MRRMMM!\n";
     std::cout << "               ~?WuxiW*`   `\"#$$$$8!!!!??!!!\n";
     std::cout << "             :X- M$$$$       `\"T#$T~!8$WUXU~\n";
-    std::cout << "            :%`  ~#$$$m:        ~!~ ?$$$$$$\n";
+    std::cout << "            :\%`  ~#$$$m:        ~!~ ?$$$$$$\n";
     std::cout << "          :!`.-   ~T$$$$8xx.  .xWW- ~\"\"##*\"\n";
     std::cout << ".....   -~~:<` !    ~?T#$$@@W@*?$$      /`\n";
     std::cout << "W$@@M!!! .!~~ !!     .:XUW$W!~ `\"~:    :\n";
-    std::cout << "#\"~~`.:x%`!!  !H:   !WM$$$$Ti.: .!WUn+!`\n";
+    std::cout << "#\"~~`.:x\%`!!  !H:   !WM$$$$Ti.: .!WUn+!`\n";
     std::cout << ":::~:!!`:X~ .: ?H.!u \"$$$B$$$!W:U!T$$M~\n";
     std::cout << ".~~   :X@!.-~   ?@WTWo(\"*$$$W$TH$! `\n";
     std::cout << "Wi.~!X$?!-~    : ?$$$B$Wu(\"**$RM!\n";
     std::cout << "$R@i.~~ !     :   ~$$$$$B$$en:``\n";
-    std::cout << "?MXT@Wx.~    :     ~\"##*$$$$M~\n";
-    std::cout << "Only one more step to go!\n";
+    std::cout << "?MXT@Wx.~    :     ~\"##*$$$$M~\n\n";
+
+    std::cout << "Only one more step to go!\n\n";
     std::cout << "You've avoided all the booby traps and security ";
     std::cout << "so there's just one more thing to do - ";
-    std::cout << "hack the terminal! \n";
+    std::cout << "hack the terminal!\n";
     std::cout << "It's all down to you!\n\n";
-    std::cout << "There are 3 numbers in the code\n";
-    std::cout << "The numbers add up to " << CodeSum << std::endl;
-    std::cout << "The numbers multiply to " << CodeProduct << std::endl;
 }
 
 // get player input
@@ -83,16 +135,6 @@ void InputPlayerGuess()
 
     std::cout << "The numbers add up to " << GuessSum << std::endl;
     std::cout << "The numbers multiply to " << GuessProduct << std::endl;
-}
-
-// check if the player got it right
-void TestWinCondition() {
-    if (GuessSum == CodeSum && GuessProduct == CodeProduct) {
-        std::cout << "Congratulations! You got it right!\n";
-        isGameWon = true;
-    } else {
-        std::cout << "That wasn't it - Time for another go...\n\n";
-    }
 }
 
 // resolve errors in the input stream

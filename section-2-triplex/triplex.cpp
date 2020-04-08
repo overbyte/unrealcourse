6b343d780fd09398597d2599429f263fd418f664
Allandt Bik-Elliott <allandt.bik-elliott@imagination.com>
2020-04-08 13:15:42 +0100

    add comments, use consts, add friendly output, take player input

--- section-2-triplex/triplex.cpp
+++ section-2-triplex/triplex.cpp
@@ -1,14 +1,19 @@
 #include <iostream>
 
 int OutputIntro();
+int OutputChallenge();
+int InputPlayerGuess();
 
 int main() 
 {
     OutputIntro();
     OutputChallenge();
+    InputPlayerGuess();
+
     return 0; 
 }
 
+// output a static intro
 int OutputIntro()
 {
     std::cout << "Only one more step to do!" << std::endl;
@@ -17,18 +22,44 @@ int OutputIntro()
     std::cout << "hack the terminal! " << std::endl;
     std::cout << "It's all down to you!" << std::endl;
     std::cout << "Enter the correct codes..." << std::endl;
+
     return 0;
 }
 
+
+// output the challenge
 int OutputChallenge()
 {
-    const int a = 3;
-    const int b = 4;
-    const int c = 9;
+    const int CodeA = 3;
+    const int CodeB = 4;
+    const int CodeC = 9;
 
-    const int sum = a + b + c;
-    const int product = a * b * c;
+    const int CodeSum = CodeA + CodeB + CodeC;
+    const int CodeProduct = CodeA * CodeB * CodeC;
 
-    std::cout << sum << std::endl;
-    std::cout << product << std::endl;
+    std::cout << "There are 3 numbers in the code" << std::endl;
+    std::cout << "The numbers add up to " << CodeSum << std::endl;
+    std::cout << "The numbers multiply to " << CodeProduct << std::endl;
+
+    return 0;
+}
+
+// get player input
+int InputPlayerGuess()
+{
+    int GuessA, GuessB, GuessC;
+
+    std::cout << "Enter your 3 numbers" << std::endl;
+    std::cin >> GuessA;
+    std::cin >> GuessB;
+    std::cin >> GuessC;
+
+    std::cout << "You entered " << GuessA << ", " << GuessB << ", " << GuessC << std::endl;
+    int GuessSum = GuessA + GuessB + GuessC;
+    int GuessProduct = GuessA * GuessB * GuessC;
+
+    std::cout << "The numbers add up to " << GuessSum << std::endl;
+    std::cout << "The numbers multiply to " << GuessProduct << std::endl;
+
+    return 0;
 }

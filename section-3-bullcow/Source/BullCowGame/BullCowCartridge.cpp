6c99e1ee70d5175ce6971079125b415ba24367cc
Allandt Bik-Elliott <allandt.bik-elliott@imagination.com>
2020-04-14 12:49:23 +0100

    add starting files from course and create a start message for the in-game terminal

---
+++ section-3-bullcow/Source/BullCowGame/BullCowCartridge.cpp
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+#include "BullCowCartridge.h"
+
+void UBullCowCartridge::BeginPlay() // When the game starts
+{
+    Super::BeginPlay();
+    PrintLine(TEXT("Welcome to the Bull Cow Game."));
+    PrintLine(TEXT("Press Tab to interact."));
+    PrintLine(TEXT("I'm thinking of a 5 letter word"));
+}
+
+void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
+{
+    ClearScreen();
+
+    const FString HiddenWord = TEXT("amiga");
+
+
+}

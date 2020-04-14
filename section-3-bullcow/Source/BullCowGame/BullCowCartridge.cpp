2f824873ac4077bacfe58f57f0ca5c98fefb49e0
Allandt Bik-Elliott <allandt.bik-elliott@imagination.com>
2020-04-14 13:39:50 +0100

    move HiddenWord into header file, add methods to make code more modular

--- section-3-bullcow/Source/BullCowGame/BullCowCartridge.cpp
+++ section-3-bullcow/Source/BullCowGame/BullCowCartridge.cpp
@@ -1,19 +1,31 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 #include "BullCowCartridge.h"
 
+
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
+
+    SetHiddenWord();
+    DisplayWelcomeMessage();
+}
+
+void UBullCowCartridge::DisplayWelcomeMessage()
+{
     PrintLine(TEXT("Welcome to the Bull Cow Game."));
     PrintLine(TEXT("Press Tab to interact."));
-    PrintLine(TEXT("I'm thinking of a 5 letter word"));
+    PrintLine(TEXT("I'm thinking of a 5 letter word")); // TODO: fix magic number
 }
+
+void UBullCowCartridge::SetHiddenWord()
+{
+    HiddenWord = TEXT("amiga");    
+}
+
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
     ClearScreen();
-
-    const FString HiddenWord = TEXT("amiga");
 
     if (Input == HiddenWord) 
     {
--- section-3-bullcow/Source/BullCowGame/BullCowCartridge.h
+++ section-3-bullcow/Source/BullCowGame/BullCowCartridge.h
@@ -12,10 +12,12 @@ class BULLCOWGAME_API UBullCowCartridge : public UCartridge
 	GENERATED_BODY()
 
 	public:
-        virtual void BeginPlay() override;
-        virtual void OnInput(const FString& Input) override;
+	virtual void BeginPlay() override;
+	virtual void OnInput(const FString& Input) override;
 
 	// Your declarations go below!
 	private:
-	
+    FString HiddenWord;
+    void SetHiddenWord();
+    void DisplayWelcomeMessage();
 };
